eed241a74e5b124624951f14e0d5b64d3daba71b
nathanxluci4 <n29436384@gmail.com>
2025-01-27 04:52:23 -0800

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '𝔫𝔞𝔱𝔥𝔞𝔫 ⛧',
+  author: '⛧',
+  botName: "⛧",
+  botOwner: '𝔫𝔞𝔱𝔥𝔞𝔫 ⛧', // Your name
+  ownerNumber: '48459234399', //Your number
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "𝘕𝘈𝘛𝘏𝘈𝘕",
+  version: "6.6.6",
+};
+
+module.exports = settings;
